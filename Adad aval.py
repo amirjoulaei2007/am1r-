99880ec434127f4b4445332e1b8c445468295518
Amir <joulaeiamirreza1020@gmail.com>
2025-12-08 16:27:49 +0330

    Add files via upload

---
+++ Adad aval.py
@@ -0,0 +1,9 @@
+n= int (input ("enter the number ? "))
+flag = False
+for i in range (2,n) :
+    if n % i == 0 :
+        flag = True 
+if flag == False : 
+    print("ADAD AVAL AST  .") 
+else :
+    print("ADAD AVA NIST  . ")  
