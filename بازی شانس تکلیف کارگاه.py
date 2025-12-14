e412998b5f8770d3234634ac56becd9bb9bd5bef
Amir <joulaeiamirreza1020@gmail.com>
2025-12-15 02:38:47 +0330

    Add files via upload

---
+++ بازی شانس تکلیف کارگاه.py
@@ -0,0 +1,13 @@
+# AmirReza Joulaei  |  Computer Engineering
+
+import random 
+z =random.randint(1,6)
+
+x = int (input ("adad? "))
+count   = 1
+while x !=  z :
+    x = int (input("adad?"))
+    count+=1
+else :
+    print("WoW!!!")
+    print(f"shoma dar {count} bar talash movafagh shodid 🤩🤩")
